175c3cd1544431e30274e88240244c7bdd819eed
swdevsmz <duke2per@gmail.com>
2024-09-04 00:34:24 +0900

    frontとbackの疎通

---
+++ front-app/src/app/services/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Todo{
+  id:number;
+  summary:string;
+  done:boolean;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiUrl = 'http://localhost:8080/api/todos';
+
+  constructor(private http: HttpClient) { }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+}
