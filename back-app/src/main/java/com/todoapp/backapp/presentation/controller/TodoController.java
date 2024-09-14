863bbb855ae5c2c6f784ab8cdad4bd636dcbea10
swdevsmz <duke2per@gmail.com>
2024-09-14 22:05:46 +0900

    リファクタリング

--- back-app/src/main/java/com/todoapp/backapp/presentation/controller/TodoController.java
+++ back-app/src/main/java/com/todoapp/backapp/presentation/controller/TodoController.java
@@ -1,13 +1,19 @@
 package com.todoapp.backapp.presentation.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import com.todoapp.backapp.business.TodoService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.todoapp.backapp.business.TodoService;
 import com.todoapp.backapp.domain.Todo;
+
+import lombok.AllArgsConstructor;
 
 
 @RestController
