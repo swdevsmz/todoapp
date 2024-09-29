42bbbba06737c8c10c9b9268fc654a836e5382c4
swdevsmz <duke2per@gmail.com>
2024-09-29 21:29:25 +0900

    aspect update

--- back-app/src/main/java/com/todoapp/backapp/aop/LoggingAspect.java
+++ back-app/src/main/java/com/todoapp/backapp/aop/LoggingAspect.java
@@ -2,22 +2,35 @@ package com.todoapp.backapp.aop;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Aspect
 @Component
+@EnableAspectJAutoProxy
 public class LoggingAspect {
     
-    Logger logger = LoggerFactory.getLogger(this.getClass());
- 
-    @After("within(com.todoapp.backapp.presentation.controller.*Controller)")
-    public void commonLog(JoinPoint jp) {
-        logger.info("メソッド実行");
-        logger.info("メソッド名" + jp.getSignature().toString());
-        logger.warn("warnログ");
+    @Before("execution(public * *..*Controller.*(..))")
+    public void before(JoinPoint jp) {
+        log.info("@ aspect before: " + jp.toString());
+    }
+
+    @After(value="execution(public * *..*Controller.*(..))")
+    public void after(JoinPoint jp) {
+        log.info("@ aspect after: " + jp.toString());
+    }
+
+    @AfterReturning(value="execution(public * *..*Controller.*(..))", returning="returnValue")
+    public void afterReturning(JoinPoint jp, Object returnValue) {
+        log.info("@ aspect afterReturning: " + returnValue);
     }
 
 }
